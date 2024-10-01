56d3dd754c0e248f55d5d1fd8ea7e460c274f660
cpowerletss <cpower@limericketss.ie>
2024-10-01 15:12:20 +0100

    Add files via upload

---
+++ List word length.py
@@ -0,0 +1,12 @@
+sample_list = [
+"apple", "dog", "hat", "sun", "blue",
+"sky", "tree", "car", "book", "pen",
+"cup", "rain", "fish", "star", "wind",
+"leaf", "ball", "bird", "ship", "frog"
+]
+
+while True:
+  word=input("Enter your word\n>")
+  print("Your word is",len(word),"letters long")
+  
+  
